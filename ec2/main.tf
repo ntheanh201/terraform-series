6045104034bcf440bc585da08bffe3ce4713030b
The Anh Nguyen <ntheanh201@gmail.com>
2022-09-14 18:09:33 +0700

    add modules

---
+++ ec2/main.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical Ubuntu AWS account id
+}
+
+resource "aws_instance" "snapify_release" {
+  count         = 2
+  ami           = data.aws_ami.ubuntu.id
+  #  instance_type = "t2.micro"
+  instance_type = var.instance_type
+  tags          = {
+    Name = "HelloWorld"
+  }
+}
+
+output "ec2" {
+  value = {
+  #    1. Use normal way
+  #    public_ip_release_1 = aws_instance.snapify_release[0].public_ip
+  #    public_ip_release_2 = aws_instance.snapify_release[1].public_ip
+  #    2. Use for expressions:
+  #    public_ip = [for v in aws_instance.snapify_release : v.public_ip]
+  #    3. Use format function
+  for i, v in aws_instance.snapify_release : format("public_ip_%d", i+1) => v.public_ip
+  }
+}
--- main.tf
+++ main.tf
@@ -1,45 +1,55 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+################################################################################
+# EC2
+################################################################################
+#module "ec2" {
+#  source = "./ec2"
 #
-#data "aws_ami" "ubuntu" {
-#  most_recent = true
-#
-#  filter {
-#    name   = "name"
-#    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#  }
-#
-#  owners = ["099720109477"] # Canonical Ubuntu AWS account id
-#}
-#
-#resource "aws_instance" "snapify_release" {
-#  count         = 2
-#  ami           = data.aws_ami.ubuntu.id
-#  #  instance_type = "t2.micro"
 #  instance_type = var.instance_type
-#  tags          = {
-#    Name = "HelloWorld"
-#  }
 #}
+
+################################################################################
+# S3
+################################################################################
+
+#module "s3" {
+#  source = "./s3"
 #
-#output "ec2" {
-#  value = {
-#  #    1. Use normal way
-#  #    public_ip_release_1 = aws_instance.snapify_release[0].public_ip
-#  #    public_ip_release_2 = aws_instance.snapify_release[1].public_ip
-#  #    2. Use for expressions:
-#  #    public_ip = [for v in aws_instance.snapify_release : v.public_ip]
-#  #    3. Use format function
-#  for i, v in aws_instance.snapify_release : format("public_ip_%d", i+1) => v.public_ip
-#  }
+#  s3_bucket_name = var.s3_bucket_name
 #}
 
-module "vpc" {
-  source = "./vpc"
+################################################################################
+# VPC
+################################################################################
 
-  vpc_cidr_block    = "10.0.0.0/16"
-  private_subnet    = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnet     = ["10.0.3.0/24", "10.0.4.0/24"]
-  availability_zone = ["eu-central-1a", "eu-central-1b"]
+#module "vpc" {
+#  source = "./vpc"
+#
+#  vpc_cidr_block    = "10.0.0.0/16"
+#  private_subnet    = ["10.0.1.0/24", "10.0.2.0/24"]
+#  public_subnet     = ["10.0.3.0/24", "10.0.4.0/24"]
+#  availability_zone = ["eu-central-1a", "eu-central-1b"]
+#}
+
+################################################################################
+# MODULES (MULTI-TIER APPLICATION)
+################################################################################
+
+locals {
+  project = "terraform-series"
+}
+
+module "networking" {
+  source = "./modules/networking"
+}
+
+module "database" {
+  source = "./modules/database"
+}
+
+module "autoscaling" {
+  source = "./modules/autoscaling"
 }
